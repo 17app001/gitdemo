90f2ab262a7917e6b54538276c6becc4ca0a394e
jerry <jerry@gmail.com>
2023-05-13 15:13:01 +0800

    完成不重複的數字輸出

--- guess.py
+++ guess.py
@@ -1,5 +1,16 @@
 import random
 
+# 1.號碼不能重複
+# 2.要排序
+
+numbers = []
+for i in range(6):
+    x = random.randint(1, 49)
+    if x not in numbers:
+        numbers.append(x)
+        print(numbers, end=" ")
+    else:
+        print(x)
 
 y = random.randint(1, 49)
 print(f"特別號:{y}")
